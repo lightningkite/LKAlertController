3301afdf99d17215364cb7d21cb8699c721dd5a6
Erik Sargent <erik@lightningkite.com>
2016-05-02 16:03:14 -0600

    Adding Carthage Support

--- Example/Pods/Target Support Files/Pods-LKAlertController_Tests-LKAlertController/Pods-LKAlertController_Tests-LKAlertController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LKAlertController_Tests_LKAlertController : NSObject
-@end
-@implementation PodsDummy_Pods_LKAlertController_Tests_LKAlertController
-@end
