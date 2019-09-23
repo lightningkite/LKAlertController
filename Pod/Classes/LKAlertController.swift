110454834952bbd79140c4e831dd4f27071df129
Brady Svedin <brady@lightningkite.com>
2019-09-23 17:10:27 -0600

    Quick fix
    Quick fix to make the pod compile.

--- Pod/Classes/LKAlertController.swift
+++ Pod/Classes/LKAlertController.swift
@@ -12,18 +12,18 @@ import UIKit
 ///Base class for creating an alert controller.
 ///Use Alert or ActionSheet instead
 public class LKAlertController {
-	///UIAlertActions callback
+    ///UIAlertActions callback
     public typealias actionHandler = (UIAlertAction?) -> Void
     
     ///Internal alert controller to present to the user
     internal var alertController: UIAlertController
-	
-	///Internal storage of view to present in
-	internal var presentationSource: UIViewController? = nil
-	
-	///Internal storage of time to delay before presenting
-	internal var delayTime: TimeInterval? = nil
-	
+    
+    ///Internal storage of view to present in
+    internal var presentationSource: UIViewController? = nil
+    
+    ///Internal storage of time to delay before presenting
+    internal var delayTime: TimeInterval? = nil
+    
     ///Internal static variable to store the override the show method for testing purposes
     internal static var alertTester: ((_ style: UIAlertController.Style, _ title: String?, _ message: String?, _ actions: [AnyObject], _ fields: [AnyObject]?) -> Void)? = nil
     
@@ -49,23 +49,23 @@ public class LKAlertController {
 
     ///Used internally to determine if the user has set the popover controller source for presenting
     internal var configuredPopoverController = false
-	
-	///Tint color for the controller
-	internal var tintColor: UIColor? {
-		didSet {
-			//Set the tint color
-			if let tint = tintColor {
-				alertController.view.tintColor = tint
-			}
-		}
-	}
+    
+    ///Tint color for the controller
+    internal var tintColor: UIColor? {
+        didSet {
+            //Set the tint color
+            if let tint = tintColor {
+                alertController.view.tintColor = tint
+            }
+        }
+    }
     
     /**
      Required Textfield - Disables primary action if textfield is empty
     */
     internal var hasRequiredTextfield = false
     internal var alertPrimaryAction: UIAlertAction?
-	
+    
     /**
     Initialize a new LKAlertController
     
@@ -83,7 +83,7 @@ public class LKAlertController {
      - parameter style:  Style of the button (.Default, .Cancel, .Destructive)
      - parameter handler:  Closure to call when the button is pressed
      */
-	@discardableResult
+    @discardableResult
     public func addAction(_ title: String, style: UIAlertAction.Style, handler: actionHandler? = nil) -> LKAlertController {
         addAction(title, style: style, preferredAction: false, handler: handler)
         
@@ -99,7 +99,7 @@ public class LKAlertController {
     - parameter preferredAction: Whether or not this action is the default action when return is pressed on a hardware keyboard
     - parameter handler:  Closure to call when the button is pressed
     */
-	@discardableResult
+    @discardableResult
     internal func addAction(_ title: String, style: UIAlertAction.Style, preferredAction: Bool = false, handler: actionHandler? = nil) -> LKAlertController {
         var action: UIAlertAction
         if let handler = handler {
@@ -122,20 +122,20 @@ public class LKAlertController {
         
         return self
     }
-	
-	///Set the view controller to present the alert in. By default this is the top controller in the window.
-	@discardableResult
-	public func presentIn(_ source: UIViewController) -> LKAlertController {
-		presentationSource = source
-		return self
-	}
-	
-	///Delay the presentation of the controller.
-	@discardableResult
-	public func delay(_ time: TimeInterval) -> LKAlertController {
-		delayTime = time
-		return self
-	}
+    
+    ///Set the view controller to present the alert in. By default this is the top controller in the window.
+    @discardableResult
+    public func presentIn(_ source: UIViewController) -> LKAlertController {
+        presentationSource = source
+        return self
+    }
+    
+    ///Delay the presentation of the controller.
+    @discardableResult
+    public func delay(_ time: TimeInterval) -> LKAlertController {
+        delayTime = time
+        return self
+    }
     
     ///Present in the view
     public func show() {
@@ -158,17 +158,17 @@ public class LKAlertController {
     - parameter completion:  Closure to call when the button is pressed
     */
     public func show(animated: Bool, completion: (() -> Void)?) {
-		//If a delay time has been set, delay the presentation of the alert by the delayTime
-		if let time = delayTime {
-			let dispatchTime = DispatchTime.now() + time
-			DispatchQueue.main.asyncAfter(deadline: dispatchTime) {
-				self.show(animated: animated, completion: completion)
-			}
-			
-			delayTime = nil
-			return
-		}
-		
+        //If a delay time has been set, delay the presentation of the alert by the delayTime
+        if let time = delayTime {
+            let dispatchTime = DispatchTime.now() + time
+            DispatchQueue.main.asyncAfter(deadline: dispatchTime) {
+                self.show(animated: animated, completion: completion)
+            }
+            
+            delayTime = nil
+            return
+        }
+        
         //Override for testing
         if let alertTester = LKAlertController.alertTester {
             alertTester(alertController.preferredStyle, title, message, alertController.actions, alertController.textFields)
@@ -187,22 +187,22 @@ public class LKAlertController {
                 let popoverController = alertController.popoverPresentationController {
                     
                     var topController = presentedController
-                    while (topController.children.last != nil) {
-                        topController = topController.children.last!
+                    while (topController.childViewControllers.last != nil) {
+                        topController = topController.childViewControllers.last!
                     }
                     
                     popoverController.sourceView = topController.view
                     popoverController.sourceRect = topController.view.bounds
             }
-			
-			//Set the presentedController to the one the user has optionally assigned
-			if let source = presentationSource {
-				presentedController = source
-			}
-			
-			DispatchQueue.main.async {
-				presentedController.present(self.alertController, animated: animated, completion: completion)
-			}
+            
+            //Set the presentedController to the one the user has optionally assigned
+            if let source = presentationSource {
+                presentedController = source
+            }
+            
+            DispatchQueue.main.async {
+                presentedController.present(self.alertController, animated: animated, completion: completion)
+            }
         }
     }
     
@@ -263,11 +263,11 @@ public class Alert: LKAlertController {
     
     - parameter title:  Title of the button
     */
-	@discardableResult
+    @discardableResult
     public func addAction(_ title: String) -> Alert {
         return addAction(title, style: .default, preferredAction: false, handler: nil)
     }
-	
+    
     /**
     Add a new button to the alert.
     
@@ -275,7 +275,7 @@ public class Alert: LKAlertController {
     - parameter style:  Style of the button (.Default, .Cancel, .Destructive)
     - parameter handler:  Closure to call when the button is pressed
     */
-	@discardableResult
+    @discardableResult
     public override func addAction(_ title: String, style: UIAlertAction.Style, handler: actionHandler? = nil) -> Alert {
         return addAction(title, style: style, preferredAction: false, handler: handler)
     }
@@ -288,7 +288,7 @@ public class Alert: LKAlertController {
      - parameter handler:  Closure to call when the button is pressed
      - parameter preferredAction: The preferred action for the user to take from an alert.
      */
-	@discardableResult
+    @discardableResult
     public override func addAction(_ title: String, style: UIAlertAction.Style, preferredAction: Bool, handler: actionHandler? = nil) -> Alert {
         return super.addAction(title, style: style, preferredAction: preferredAction, handler: handler) as! Alert
     }
@@ -298,10 +298,10 @@ public class Alert: LKAlertController {
     
     - parameter textField:  textField to add to the alert (must be a var, not let)
     */
-	@discardableResult
+    @discardableResult
     public func addTextField( _ textField: inout UITextField, required: Bool = false) -> Alert {
-		var field: UITextField?
-		
+        var field: UITextField?
+        
         alertController.addTextField { [unowned textField] (tf: UITextField!) -> Void in
             tf.text = textField.text
             tf.placeholder = textField.placeholder
@@ -324,31 +324,31 @@ public class Alert: LKAlertController {
             self.hasRequiredTextfield = true
         }
         
-		if let field = field {
-			textField = field
-		}
+        if let field = field {
+            textField = field
+        }
         
         return self
     }
-	
-	///Set the view controller to present the alert in. By default this is the top controller in the window.
-	@discardableResult
-	public override func presentIn(_ source: UIViewController) -> Alert {
-		return super.presentIn(source) as! Alert
-	}
-	
-	///Delay the presentation of the controller.
-	@discardableResult
-	public override func delay(_ time: TimeInterval) -> Alert {
-		return super.delay(time) as! Alert
-	}
-	
-	///Set the tint color for the alert
-	@discardableResult
-	public func tint(_ color: UIColor) -> Alert {
-		tintColor = color
-		return self
-	}
+    
+    ///Set the view controller to present the alert in. By default this is the top controller in the window.
+    @discardableResult
+    public override func presentIn(_ source: UIViewController) -> Alert {
+        return super.presentIn(source) as! Alert
+    }
+    
+    ///Delay the presentation of the controller.
+    @discardableResult
+    public override func delay(_ time: TimeInterval) -> Alert {
+        return super.delay(time) as! Alert
+    }
+    
+    ///Set the tint color for the alert
+    @discardableResult
+    public func tint(_ color: UIColor) -> Alert {
+        tintColor = color
+        return self
+    }
     
     ///Shortcut method for adding an Okay button and showing the alert
     public func showOkay() {
@@ -403,7 +403,7 @@ public class ActionSheet: LKAlertController {
     
     - parameter title:  Title of the button
     */
-	@discardableResult
+    @discardableResult
     public func addAction(_ title: String) -> ActionSheet {
         return addAction(title, style: .cancel, handler: nil)
     }
@@ -415,28 +415,28 @@ public class ActionSheet: LKAlertController {
     - parameter style:  Style of the button (.Default, .Cancel, .Destructive)
     - parameter handler:  Closure to call when the button is pressed
     */
-	@discardableResult
+    @discardableResult
     public override func addAction(_ title: String, style: UIAlertAction.Style, handler: actionHandler? = nil) -> ActionSheet {
         return super.addAction(title, style: style, preferredAction: false, handler: handler) as! ActionSheet
     }
-	
-	///Set the view controller to present the alert in. By default this is the top controller in the window.
-	@discardableResult
-	public override func presentIn(_ source: UIViewController) -> ActionSheet {
-		return super.presentIn(source) as! ActionSheet
-	}
-	
-	//Delay the presentation of the controller.
-	@discardableResult
-	open override func delay(_ time: TimeInterval) -> ActionSheet {
-		return super.delay(time) as! ActionSheet
-	}
-	
-	///Set the tint color for the action sheet
-	public func tint(_ color: UIColor) -> ActionSheet {
-		tintColor = color
-		return self
-	}
+    
+    ///Set the view controller to present the alert in. By default this is the top controller in the window.
+    @discardableResult
+    public override func presentIn(_ source: UIViewController) -> ActionSheet {
+        return super.presentIn(source) as! ActionSheet
+    }
+    
+    //Delay the presentation of the controller.
+    @discardableResult
+    open override func delay(_ time: TimeInterval) -> ActionSheet {
+        return super.delay(time) as! ActionSheet
+    }
+    
+    ///Set the tint color for the action sheet
+    public func tint(_ color: UIColor) -> ActionSheet {
+        tintColor = color
+        return self
+    }
     
     /**
     Set the presenting bar button item. Used for presenting the action sheet on iPad.
@@ -444,7 +444,7 @@ public class ActionSheet: LKAlertController {
     
     - parameter item:  UIBarButtonItem that the action sheet will present from
     */
-	@discardableResult
+    @discardableResult
     public func setBarButtonItem(_ item: UIBarButtonItem) -> ActionSheet {
         if let popoverController = alertController.popoverPresentationController {
             popoverController.barButtonItem = item
@@ -460,7 +460,7 @@ public class ActionSheet: LKAlertController {
     
     - parameter source:  The view the action sheet will present from
     */
-	@discardableResult
+    @discardableResult
     public func setPresentingSource(_ source: UIView) -> ActionSheet {
         if let popoverController = alertController.popoverPresentationController {
             popoverController.sourceView = source
